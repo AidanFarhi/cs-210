cafec613bfb287f3f5d5dfd4a869e7ba0a464546
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-02-16 22:01:05 -0500

    finished int vs string lab

--- labs/int_vs_string.cpp
+++ labs/int_vs_string.cpp
@@ -1,26 +1,44 @@
 #include <string>
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main() 
 {
+    // Initialize variables.
     string inputName;
     int age;
-    
-    // Set exception mask for cin stream
+
+    // Set exception mask for cin stream.
     cin.exceptions(ios::failbit);
 
+    // Read in the first name of input.
     cin >> inputName;
+
+    // Loop through all inputs until -1 is encountered.
     while(inputName != "-1") 
     {
-        // FIXME: The following line will throw an ios_base::failure.
-        //        Insert a try/catch statement to catch the exception.
-        //        Clear cin's failbit to put cin in a useable state.
-        cin >> age;
+        // Catch any invalid ages.
+        try
+        {
+            cin >> age;
+        }
+        catch(const exception& e)
+        {
+            // Set age to -1, clear error bit, and discard invalid input.
+            age = -1;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        // Output name and age.
         cout << inputName << " " << (age + 1) << endl;
+
+        // Get next name from input.
         cin >> inputName;
     }
 
+    // Exit program.
     return 0;
 }
